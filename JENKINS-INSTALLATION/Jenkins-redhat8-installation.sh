8c9e12768b3589d9c23125ccdbf388d53fb19117
Gabiham1977 <dhammondg@gmail.com>
2024-10-18 05:10:50 -0500

    updated jenkins installation

--- JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
@@ -13,7 +13,7 @@ sudo hostnamectl set-hostname jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
+sudo yum upgrade-y
 # Add required dependencies for the jenkins package
 sudo yum install java-11-openjdk
 sudo yum install jenkins
